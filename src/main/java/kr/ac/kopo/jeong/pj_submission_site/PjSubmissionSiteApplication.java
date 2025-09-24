073ab4855fbb66a0430bef6c76a94d10203a4928
a71143055 <a71143055@gmail.com>
2025-09-24 19:26:19 +0900

    PJ

--- src/main/java/kr/ac/kopo/jeong/pj_submission_site/PjSubmissionSiteApplication.java
+++ src/main/java/kr/ac/kopo/jeong/pj_submission_site/PjSubmissionSiteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PjSubmissionSiteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PjSubmissionSiteApplication.class, args);
     }
-
 }
